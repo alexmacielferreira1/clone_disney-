7266ceadc1b83849aafc8014ff2b581d8711f05f
alexmacielferreira1 <alexmacielferreira@gmail.com>
2025-06-16 19:39:32 -0300

    atualizacao

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,12 @@ function styles(){
         .pipe(gulp.dest("./dist/css"));
 }
 
+function images(){
+    return gulp.src("./src/images/**")
+        .pipe(sass({outputStyle: "compressed"}))
+        .pipe(gulp.dest("./dist/css"));
+}
+
 exports.default = styles;
 exports.watch = function(){
     gulp.watch("./src/styles/*.scss", gulp.parallel(styles))
