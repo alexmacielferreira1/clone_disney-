e55fd39383d25515ea54787261f7272ef53708c5
alexmacielferreira1 <alexmacielferreira@gmail.com>
2025-06-23 10:01:39 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+const header = document.querySelector('.header');
+const alturaHero = document.querySelector('.hero').offsetHeight;
+
+window.addEventListener('scroll', function () {
+    const posicaoAtual = window.scrollY;
+
+    if (posicaoAtual < alturaHero) {
+        // Ainda dentro do .hero → esconde
+        ocultaElementoDoHeader();
+    } else {
+        // Passou do .hero → mostra
+        exibeElementoDoHeader();
+    }
+});
+
+function ocultaElementoDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementoDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -12,7 +38,18 @@ document.addEventListener('DOMContentLoaded', function () {
             botao.target.classList.add('shows__list--is--active');
         });
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 });
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
